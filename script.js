e7d95c50c404bba5a92633d78a2c45a846f6e98d
Ezequiel Kuchar <ezequiel.kuchar@gmail.com>
2024-07-04 12:36:59 +0200

    Added grid logic and UI

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const input = document.querySelector("#input");
-const value = document.querySelector("#value");
-value.textContent = input.value;
-input.addEventListener("input", (event) => {
-  value.textContent = event.target.value;
+function initializeGrid() {
+    const A = input.value;
+    const gridContainer = document.querySelector(".container")
+    gridContainer.textContent = "";
+
+    const gridSize = 100 / A + "%";
+
+    for (let i = 1; i < (A * A) + 1; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.setAttribute("id", i)
+        gridContainer.appendChild(gridItem);
+        gridItem.style.width = gridSize;
+        gridItem.style.height = gridSize;
+
+        gridItem.addEventListener("mouseover", function () {
+            gridItem.style.backgroundColor = "black";
+        });
+    }
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.querySelector("#input");
+    const value = document.querySelector("#value");
+    const resetButton = document.querySelector("#resetButton");
+
+    value.textContent = input.value;
+
+    input.addEventListener("input", function (event) {
+        value.textContent = event.target.value;
+        initializeGrid();
+    });
+
+    resetButton.addEventListener("click", function() {
+        initializeGrid()
+    })
 });
+
+initializeGrid();
+
+
